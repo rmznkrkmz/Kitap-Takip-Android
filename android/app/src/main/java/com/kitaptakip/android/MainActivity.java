2e2782eabcb334a37939f9791f41fae226060644
rmznkrkmz <rmznkrkmz@gmail.com>
2025-12-10 22:00:00 +0300

    new3

---
+++ android/app/src/main/java/com/kitaptakip/android/MainActivity.java
@@ -0,0 +1,5 @@
+package com.kitaptakip.android;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- android/app/src/main/java/com/rmc/kitaptakipandroid/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.rmc.kitaptakipandroid;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
